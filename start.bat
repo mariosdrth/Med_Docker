151e3a854295ec7757a20c2d29ad7dd4befa5be1
Marios Papamarkou <mariosdrth@yahoo.gr>
2019-09-23 23:22:40 +0200

    Fix batch

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
+kubectl apply -f dev-namespace.yaml
 kubectl config set-context --current --namespace=dev
 
-kubectl apply -f dev-namespace.yaml
 kubectl apply -f service-client.yaml
 kubectl apply -f service-server.yaml
 kubectl apply -f conf-server.yaml
